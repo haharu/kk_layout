d6a3c9c4db75e7604db7c4c15d2829c3e5ba45cd
haharuLin <haharu.ame@gmail.com>
2016-12-22 22:10:24 +0800

    added server online mode

--- server.js
+++ server.js
@@ -4,47 +4,35 @@ import path from 'path';
 import kwdm from 'koa-webpack-dev-middleware';
 import kwhm from 'koa-webpack-hot-middleware';
 import devConfig from './webpack-dev-server.config.js';
-import prodConfig from './webpack-production.config.js';
+
 import Koa from 'koa';
 import convert from 'koa-convert';
+import serve from 'koa-static';
 
 let PORT = process.env.PORT || 3000;
-let NODE_ENV = process.env.NODE_ENV  || 'development';
+let NODE_ENV = process.env.NODE_ENV || 'development';
 
 const app = new Koa();
-const compile = webpack(NODE_ENV === 'production' && prodConfig || devConfig);
+const compile = webpack(devConfig);
 
 const _use = app.use
 app.use = x => _use.call(app, convert(x))
 
-app.use(kwdm(compile, {
-    // display no info to console (only warnings and errors)
-    noInfo: false,
+if (NODE_ENV === 'development') {
+    app.use(kwdm(compile, {
+        noInfo: false,
+        quiet: false,
+        lazy: false,
+        publicPath: path.join(__dirname, 'build'),
+        stats: {
+            colors: true
+        }
+    }));
 
-    // display nothing to the console
-    quiet: false,
-
-    // switch into lazy mode
-    // that means no watching, but recompilation on every request
-    lazy: true,
-
-    // public path to bind the middleware to
-    // use the same as in webpack
-    // publicPath: path.join(__dirname, 'build'),
-
-    // custom headers
-    // headers: {
-    //     'Access-Control-Allow-Origin': '*'
-    // },
-
-    // options for formating the statistics
-    stats: {
-        colors: true
-    }
-}));
-
-app.use(kwhm(compile));
-
+    app.use(kwhm(compile));
+} else {
+    app.use(serve('public'));
+}
 
 // set the initial content
 app.use(async(ctx, next) => {
