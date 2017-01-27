b8e84cd52c7f9d82b50d456d820096f9641c2309
haharuLin <haharu.ame@gmail.com>
2017-01-27 20:50:21 +0800

    extract NavLink

--- app/src/components/Header.jsx
+++ app/src/components/Header.jsx
@@ -1,48 +1,7 @@
 'use strict';
 
 import React, {Component, PropTypes} from 'react';
-import {Link, IndexLink} from 'react-router';
-
-class NavLink extends Component {
-    constructor(props, context) {
-        super(props, context)
-    }
-
-    render() {
-        const {index, to, children, activeClassName} = this.props
-        const {router} = this.context
-
-        const isActive = index
-            ? router.isActive(to, index)
-            : router.isActive(to)
-        const LinkComponent = index
-            ? IndexLink
-            : Link
-
-        return (
-            <li className={isActive
-                ? activeClassName
-                : ''}>
-                <LinkComponent to={to}>{children}</LinkComponent>
-            </li>
-        )
-    }
-}
-
-NavLink.contextTypes = {
-    router: React.PropTypes.object.isRequired
-}
-
-class SubNavLink extends Component {
-    render() {
-        const {to, name} = this.props
-        return (
-            <Link to={to} className="nav-item is-tab" activeClassName="is-active">
-                {name}
-            </Link>
-        )
-    }
-}
+import {NavLink, SubNavLink} from './NavLink'
 
 export default class Header extends Component {
     constructor(props) {
