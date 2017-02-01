3cf9c8c0af056d53bb6bd911f96e02d42f9d0257
haharuLin <haharu.ame@gmail.com>
2017-02-02 01:51:36 +0800

    fix leaflet universal in diff way

--- app/src/components/Leaflet.jsx
+++ app/src/components/Leaflet.jsx
@@ -1,35 +1,50 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
-import L from 'leaflet';
 
-delete L.Icon.Default.prototype._getIconUrl;
-
-let defaultIcon = {
-    iconRetinaUrl: require('../../../node_modules/leaflet/dist/images/marker-icon-2x.png'),
-    iconUrl: require('../../../node_modules/leaflet/dist/images/marker-icon.png'),
-    shadowUrl: require('../../../node_modules/leaflet/dist/images/marker-shadow.png')
-}
-
-L.Icon.Default.mergeOptions(defaultIcon);
-
-import {Map, Marker, Popup, TileLayer} from 'react-leaflet-universal';
+let Map;
 
 export default class Leaflet extends Component {
     constructor(props) {
         super(props)
     }
 
+    componentDidMount() {
+        Map = require('react-leaflet').Map
+        this.forceUpdate();
+    }
+
     render() {
-        const position = [40.7309784, -73.9956433];
-        return (
-            <Map center={position} zoom={13}>
-                <TileLayer url='http://{s}.tile.osm.org/{z}/{x}/{y}.png' attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'/>
-                <Marker position={position}>
-                    <Popup>
-                        <span>test</span>
-                    </Popup>
-                </Marker>
-            </Map>
-        );
+        if (Map) {
+
+            const L = require('leaflet');
+
+            delete L.Icon.Default.prototype._getIconUrl;
+
+            let defaultIcon = {
+                iconRetinaUrl: require('../../../node_modules/leaflet/dist/images/marker-icon-2x.png'),
+                iconUrl: require('../../../node_modules/leaflet/dist/images/marker-icon.png'),
+                shadowUrl: require('../../../node_modules/leaflet/dist/images/marker-shadow.png')
+            }
+
+            L.Icon.Default.mergeOptions(defaultIcon);
+
+            const position = [40.7309784, -73.9956433];
+            const url = 'http://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}'
+            const attribution = '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            const {Marker, Popup, TileLayer} = require('react-leaflet');
+
+            return (
+                <Map center={position} zoom={13}>
+                    <TileLayer url={url} attribution={attribution}/>
+                    <Marker position={position}>
+                        <Popup>
+                            <span>test</span>
+                        </Popup>
+                    </Marker>
+                </Map>
+            );
+        } else {
+            return (null)
+        }
     }
 }
