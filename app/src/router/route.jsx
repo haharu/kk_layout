dd0cd3d01601f3265b1762acdc644f24bcc89e54
haharuLin <haharu.ame@gmail.com>
2017-03-05 13:42:16 +0800

    removed useless component

--- app/src/router/route.jsx
+++ app/src/router/route.jsx
@@ -1,28 +1,11 @@
 import React from 'react'
 import {Route, Link, IndexRedirect, IndexRoute} from 'react-router'
 import App, {PageNotFound, Home} from '../containers/App'
-import Map from '../containers/Map'
-import Temp, {Temp1, Temp2, Temp3, Temp4, TempMap} from '../containers/Temp'
-import {Locate} from '../components/LeftCard'
 import Header from '../components/Header'
 
 export default(
     <Route path="/" component={App}>
         <IndexRoute component={Home}/>
-        <Route path="bmap" component={Map}>
-            <IndexRedirect to="/bmap/locate"/>
-            <Route path="locate" component={Locate}/>
-            <Route path="*" component={PageNotFound}/>
-        </Route>
-        <Route path="temp-page" component={Temp}>
-            <IndexRedirect to="/temp-page/1"/>
-            <Route path="/temp-page/1" component={Temp1}/>
-            <Route path="/temp-page/2" component={Temp2}/>
-            <Route path="/temp-page/3" component={Temp3}/>
-            <Route path="/temp-page/4" component={Temp4}/>
-            <Route path="*" component={PageNotFound}/>
-        </Route>
-        <Route path="temp-map" component={TempMap}/>
         <Route path="*" component={() => (
             <div><Header/><PageNotFound/></div>
         )}/>
