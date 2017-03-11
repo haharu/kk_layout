3bcd3597d822cec678f9d16cbe1142f21711378d
haharuLin <haharu.ame@gmail.com>
2017-03-11 22:19:42 +0800

    removed useless testcase

--- app/test/reducer.spec.js
+++
@@ -1,9 +0,0 @@
-import chai from 'chai'
-import rootReducer from '../src/app/reducers'
-
-describe('Reducers', function() {
-	it('must return a function to reduce actions', function() {
-		chai.assert.isFunction(rootReducer);
-		chai.assert.strictEqual(rootReducer.length, 1);
-	})
-})
