b702b782799f8eefcf5869d376e298f3783d5cb0
haharuLin <haharu.ame@gmail.com>
2017-03-16 01:17:20 +0800

    update package.json

--- app/src/helpers/Html.jsx
+++ app/src/helpers/Html.jsx
@@ -27,7 +27,6 @@ export default class Html extends Component {
                     {head.script.toComponent()}
                     <link rel="shortcut icon" href="/favicon.ico"/>
                     <meta name="viewport" content="width=device-width, initial-scale=1"/>
-                    <script src="https://api.map.baidu.com/api?v=2.0&ak=SzMcb4lxi6wiWMqQK3svjEXzWFCB8uWu&s=1" charSet="utf-8"></script>
                     {Object.keys(assets.styles).map((style, key) => <link href={assets.styles[style]} key={key} media="screen, projection" rel="stylesheet" type="text/css" charSet="UTF-8"/>)}
                 </head>
                 <body>
