306300886492ac4c7f49886fb6cb44fa56250bac
haharuLin <haharu.ame@gmail.com>
2017-03-16 02:47:38 +0800

    using chai assert instead of native assert

--- test/index.js
+++ test/index.js
@@ -1,4 +1,4 @@
-import { assert } from 'assert'
+import { assert } from 'chai'
 describe('Array', function() {
 	describe('#indexOf()', function() {
 		it('should return -1 when the value is not present', function() {
